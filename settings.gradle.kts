09a7d27adbd9143003c809e42e7891a5f6b62b27
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-04-15 20:55:54 +0530

    Commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,10 +22,7 @@ dependencyResolutionManagement {
                 create<BasicAuthentication>("basic")
             }
             credentials {
-                // Do not change the username below.
-                // This should always be `mapbox` (not your username).
                 username = "mapbox"
-                // Use the secret token you stored in gradle.properties as the password
                 password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
             }
         }
